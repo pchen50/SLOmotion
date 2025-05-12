b043ce0a1c19a3a66f17bbc3c30ae1182c1ca45d
noahscott23 <nscott05@calpoly.edu>
2025-05-12 00:01:12 -0700

    ratings.py

--- src/api/ratings.py
+++ src/api/ratings.py
@@ -13,6 +13,7 @@ router = APIRouter(
 
 
 class Rating(BaseModel):
+    user_id: int
     movie_id: int
     status: str
     rating: int
@@ -20,4 +21,23 @@ class Rating(BaseModel):
 
 @router.get("/{movie_id}", response_model=List[Rating])
 def get_movie_ratings(movie_id: int) -> List[Rating]:
-    pass
+    with db.engine.connect() as conn:
+        results = conn.execute(sqlalchemy.text(
+            """
+            SELECT user_id, movie_id, status, rating, notes
+            FROM movie_ratings
+            WHERE movie_id = :movie_id
+            """
+        ), {"movie_id": movie_id}).mappings().all()
+
+        ratings = []
+        for row in results:
+            ratings.append(Rating(
+                user_id = row.user_id,
+                movie_id = row.movie_id,
+                status = row.status,
+                rating = row.rating,
+                notes = row.notes
+            ))
+
+        return ratings
